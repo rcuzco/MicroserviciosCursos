b69002e51db9eddcde0d72931945e32f49e9df82
Rony Cuzco <ronald.cuzco@ciberexperis.es>
2024-07-28 12:06:16 +0200

    - Se quita parámetro sin usar

--- FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace FrontEnd.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {            
         }
 
